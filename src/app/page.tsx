3dde0b3e498c8d55c658c6f178e5df6b9e79bbda
D Synkiw <dsynkiw@rrc.ca>
2026-05-11 13:05:11 -0500

    created a proper landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
-import Link from 'next/link'
-
 export default function Home() {
   return (
     <>
+      <h1 className="text-center m-5">Welcome to my Recipe Browser project!</h1>
+      <p className="m-3">
+        The goal of this project is for me to learn Next.js in an interactive way. I am following this document:{' '}
+        <a href="/recipe-manager-project-spec.md" className="text-blue-500" download>
+          Recipe Manager Project
+        </a>
+      </p>
+      <p className="m-3">This self-directed learning project will build your competency in modern full-stack web 
+        development using React 19, Next.js 16+ (App Router), PostgreSQL, and authentication. 
+        You'll create a recipe sharing platform where users can browse recipes publicly and create 
+        their own recipes after authentication.</p>
     </>
   );
 }
